73ab388ad22bcda8fb4960fb2bca7bfadbc4d779
HP <HP@192.168.15.7>
2021-05-03 11:16:29 -0300

    Primeiro commit

---
+++ tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -0,0 +1,20 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		
+		// retorna na Classe de test arredondamento para cima se for numero quebrado
+		return valor.setScale(2, RoundingMode.HALF_UP);
+	}
+
+}
---
+++ tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,19 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class ReajusteService {
+
+	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+
+		// Refaturação Solicita para o Enum classe Desempenho para enviar o percentual
+		BigDecimal percentual = desempenho.percentualReajuste();
+		BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+		funcionario.reajustarSalario(reajuste);
+
+	}
+
+}
